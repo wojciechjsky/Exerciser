abea76cf6abd65c1c552c4867d0d8993a0635b3a
wojciechjsky <jablonski91w@gmail.com>
2021-05-20 13:15:07 +0200

    JSON file data + extract all user IDs

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import { MathCustom as MC } from "./resources/example"
-
+import * as DB from './resources/data.json'
 
 const MathCustom = new MC();
 
 const r = MathCustom.multiplyBy2(2)
 const r1 = MathCustom.divideBy2(4);
 const r2 = MathCustom.squarePower(4);
+
+const IDs = DB.data.map(e => e.Id)
+
+console.log(IDs);
+
+// const ids = data.forEach( e => e.name)
