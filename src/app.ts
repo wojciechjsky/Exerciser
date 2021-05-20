21021e0ba5c97e0ada3711fbe08912f27ca46061
wojciechjsky <jablonski91w@gmail.com>
2021-05-20 12:43:46 +0200

    Minify with ts

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import { MathCustom as MC } from "./resources/example"
+
+
+const MathCustom = new MC();
+
+const r = MathCustom.multiplyBy2(2)
+const r1 = MathCustom.divideBy2(4);
+const r2 = MathCustom.squarePower(4);
